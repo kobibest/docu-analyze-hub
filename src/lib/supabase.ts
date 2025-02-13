c50ee7694f60ed342bbf1d6f12b47aeba88978f5
kobibest <kobi@eitan-m.co.il>
2025-02-13 17:21:23 +0200

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://your-project-url.supabase.co";
-const supabaseAnonKey = "your-anon-key";
+const supabaseUrl = "https://ntnjhnpxzsnkmiypfjjj.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50bmpobnB4enNua21peXBmampqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzNTU0MjYsImV4cCI6MjA1NDkzMTQyNn0.TFXfcZxZB1S5ye0DPl6ALHY_nNQ8k_dsD-U_Wgb1y90";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Missing Supabase URL or Anon Key");
